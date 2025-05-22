8eb180d8b86c76dd439e8b78bc48fe6e5bb651f9
Alwi Akbar <alwiakbar09@gmail.com>
2025-05-22 20:02:35 +0700

    feat : add console.log new data

--- login.js
+++ login.js
@@ -4,3 +4,25 @@ console.log("Login Feature");
 console.log("Forget Pass?")
 
 //Step for git
+// For first time
+/*
+git init
+
+git add fileName or git add. -> tambahkan semua file
+
+git commit -m "message"
+
+git remote add origin "Link Repositori"
+
+git push origin main
+*/
+
+//second time
+/*
+git add fileName or git add. -> tambahkan semua file
+
+git commit -m "message"
+
+git push origin main
+
+*/
